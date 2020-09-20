a947fb94af591649970c3d27bf305d193c8cafe9
jindada1 <zou_406143883@qq.com>
2020-09-20 10:37:18 +0800

    replace iview with element

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,12 +1,46 @@
-
 new Vue({
     el: '#app',
-    data: {
-        visible: false
+    data: function () {
+        return {
+            editableTabsValue: '2',
+            editableTabs: [{
+                title: 'Tab 1',
+                name: '1',
+                content: 'Tab 1 content'
+            }, {
+                title: 'Tab 2',
+                name: '2',
+                content: 'Tab 2 content'
+            }],
+            tabIndex: 2
+        }
     },
     methods: {
-        show: function () {
-            this.visible = true;
+        addTab(targetName) {
+            let newTabName = ++this.tabIndex + '';
+            this.editableTabs.push({
+                title: 'New Tab',
+                name: newTabName,
+                content: 'New Tab content'
+            });
+            this.editableTabsValue = newTabName;
+        },
+        removeTab(targetName) {
+            let tabs = this.editableTabs;
+            let activeName = this.editableTabsValue;
+            if (activeName === targetName) {
+                tabs.forEach((tab, index) => {
+                    if (tab.name === targetName) {
+                        let nextTab = tabs[index + 1] || tabs[index - 1];
+                        if (nextTab) {
+                            activeName = nextTab.name;
+                        }
+                    }
+                });
+            }
+
+            this.editableTabsValue = activeName;
+            this.editableTabs = tabs.filter(tab => tab.name !== targetName);
         }
     }
 })
